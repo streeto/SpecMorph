4cea0bf4c0c2483b5c3621f36abcb7a4adf78b66
André Luiz de Amorim <streetomon@gmail.com>
2014-09-22 09:59:17 -0300

    PSF test: added plot of fwhm and beta vs. distance to image center.

--- test/psf_plot.py
+++ test/psf_plot.py
@@ -23,6 +23,18 @@ galaxiesV1200 = ['NGC0023',
                  'NGC0447',
                  'NGC6762']
 
+psfcenter = {'NGC0023': (14, 16),
+             'NGC2916': (45, 32),
+             #'NGC0180': (62, 57),
+             'IC1652': (52, 36),
+             'NGC0447': (22, 16),
+             'NGC5473': (45, 14),
+             'NGC5557': (18, 24),
+             'NGC5602': (40, 23),
+             'NGC5642': (25, 18),
+             'NGC6762': (22, 20),
+             }
+
 param_dtype = [('lambda', 'float64'), ('I_0', 'float64'),
                ('fwhm', 'float64'), ('beta', 'float64'), 
                ('x0', 'float64'), ('y0', 'float64'),
@@ -45,7 +57,7 @@ y0V1200 = np.ma.empty((len(galaxiesV1200),  nlambdaV1200))
 ellV1200 = np.ma.empty((len(galaxiesV1200),  nlambdaV1200))
 
 for i, galaxy in enumerate(galaxiesV500):
-    cube = '%s.%s.v1.5.PSF.dat' % (galaxy, 'V500')
+    cube = 'psf/%s.%s.v1.5.PSF.dat' % (galaxy, 'V500')
     p = np.genfromtxt(cube, dtype=param_dtype)
     wlV500 = p['lambda']
     mask = p['flag'] | (p['good'] < 0.6)
@@ -61,7 +73,7 @@ for i, galaxy in enumerate(galaxiesV500):
     ellV500[i, mask] = np.ma.masked
 
 for i, galaxy in enumerate(galaxiesV1200):
-    cube = '%s.%s.v1.5.PSF.dat' % (galaxy, 'V1200')
+    cube = 'psf/%s.%s.v1.5.PSF.dat' % (galaxy, 'V1200')
     p = np.genfromtxt(cube, dtype=param_dtype)
     wlV1200 = p['lambda']
     mask = p['flag'] | (p['good'] < 0.7)
@@ -79,6 +91,16 @@ for i, galaxy in enumerate(galaxiesV1200):
 
 wlmin = wlV1200.min()
 wlmax = wlV500.max()
+imcenter = (36, 38.5)
+
+distV500 = []
+for g in galaxiesV500:
+    x = psfcenter[g]
+    distV500.append(np.sqrt((x[0] - imcenter[0])**2 + (x[1] - imcenter[1])**2))
+distV1200 = []
+for g in galaxiesV1200:
+    x = psfcenter[g]
+    distV1200.append(np.sqrt((x[0] - imcenter[0])**2 + (x[1] - imcenter[1])**2))
 
 plt.clf()
 plt.subplot(211)
@@ -107,3 +129,26 @@ plt.ylim(0.0, 4.0)
 plt.xlim(wlmin, wlmax)
 
 plt.savefig('PSF_all.png')
+
+
+plt.figure(figsize=(5,6))
+plt.clf()
+
+plt.subplot(211)
+plt.plot(distV500, fwhmV500.mean(axis=1), 'ro', label='V500')
+plt.plot(distV1200, fwhmV1200.mean(axis=1), 'bo', label='V1200')
+plt.ylabel(r'FWHM $[arcsec]$')
+#plt.xlabel(r'distance $[arcsec]$')
+plt.gca().set_xticklabels([])
+plt.ylim(0.0,5.0)
+plt.xlim(0.0,35.0)
+plt.legend(loc='lower right')
+
+plt.subplot(212)
+plt.plot(distV500, betaV500.mean(axis=1), 'ro', label='V500')
+plt.plot(distV1200, betaV1200.mean(axis=1), 'bo', label='V1200')
+plt.ylabel(r'$\beta$')
+plt.xlabel(r'distance $[arcsec]$')
+plt.ylim(0.0,5.0)
+plt.xlim(0.0,35.0)
+plt.savefig('PSF_distance.png')
