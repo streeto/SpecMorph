6ca8758fbb8336a6ebf6d8865c0508779648bf8c
André Luiz de Amorim <streetomon@gmail.com>
2014-11-18 08:13:15 -0200

    PSF test: updated field stars fitting.

--- test/psf/psf.py
+++ test/psf/psf.py
@@ -37,11 +37,19 @@ galaxycenter = {'NGC0023': (31.4, 35.6),
                 }
 
 galaxy = sys.argv[1]
-cubeversion = sys.argv[2]
-cube = '../../../cubes.DR2/%s.%s.rscube.fits.gz' % (galaxy, cubeversion)
+grating = sys.argv[2]
+func = sys.argv[3] # 'Kolmogorov', 'Moffat, 'Gaussian'
+beta4 = len(sys.argv) > 4 and sys.argv[4] == 'beta4'
+if beta4:
+    name = '%sBeta4' % (func)
+else:
+    name = func
+
+cube = '../../../cubes.DR2/%s.%s.rscube.fits.gz' % (galaxy, grating)
 psfradius = 7
-psfLstep = 40.0 # /AA
+psfLstep = 400.0 # /AA
 badpix_frac = 0.5
+sigma2fwhm = 2.0 * np.sqrt(2.0 * np.log(2.0))
 
 debug = False
 i_plot = 3
@@ -58,20 +66,25 @@ psfLstep_ix = int(np.ceil(psfLstep / dlambda))
 wl_ix = np.arange(0, len(wl), psfLstep_ix)
 flux = np.ma.empty((len(wl_ix), _flux.shape[1], _flux.shape[2]))
 mask = np.ma.empty((len(wl_ix), _flux.shape[1], _flux.shape[2]), dtype='bool')
+psf_wl = np.ma.empty((len(wl_ix)))
 for i, j in enumerate(wl_ix):
-    print 'Computing binned image %d of %d (@ %.1f \\AA)' % (i, len(wl_ix), wl[j])
-    l_radius = psfLstep_ix / 2
-    j1 = j - l_radius if j > (0 + l_radius) else 0
-    j2 = j + l_radius if j < (wl_ix[-1] - l_radius) else wl_ix[-1]
-    mask[i] = _mask[j1:j2].sum(axis=0) > badpix_frac * (j2 - j1)
-    flux[i] = np.ma.median(_flux[j1:j2], axis=0)
+    j1 = j
+    j2 = j + psfLstep_ix
+    if j2 >= len(wl): j2 = len(wl) - 1
+    jc = (j1 + j2) / 2
+    print 'Computing binned image %d of %d (@ %.1f \\AA)' % (i, len(wl_ix), wl[jc])
+    max_npts = psfLstep_ix
+    masked_pts = _flux.mask[j1:j2].sum(axis=0)
+    mask = masked_pts > (badpix_frac * max_npts)
+    flux[i] = np.ma.mean(_flux[j1:j2], axis=0)
+    flux[i, mask] = np.ma.masked
+    psf_wl[i] = wl[jc]
 
 psfYrange = slice(psfcenter[galaxy][0] - psfradius, psfcenter[galaxy][0] + psfradius + 1)
 psfXrange = slice(psfcenter[galaxy][1] - psfradius, psfcenter[galaxy][1] + psfradius + 1)
 
 psfflux = flux[:,psfYrange, psfXrange].copy()
 fill_flux = flux
-psf_wl = wl[wl_ix]
 
 if debug:
     plt.clf()
@@ -138,33 +151,115 @@ if debug:
     plt.plot(bins_c, bg_r)
     plt.show()
 
-moffat = function_description('Moffat')
-moffat.PA.setValue(60, vmin=0, vmax=180)
-moffat.ell.setValue(0.2, vmin=0.0, vmax=0.4)
-moffat.I_0.setValue(1.0, vmin=1e-20, vmax=10.0)
-moffat.fwhm.setValue(2.0, vmin=1e-20, vmax=10.0)
-moffat.beta.setValue(1.1, vmin=1e-20, vmax=10.0)
+psf_func = function_description(func, 'psf')
+psf_func.PA.setValue(0.0, vmin=-190, vmax=190)
+psf_func.ell.setValue(0.2, vmin=-1.0, vmax=1.0)
+psf_func.I_0.setValue(1.0, vmin=1e-20, vmax=10.0)
+if func == 'Gaussian':
+    psf_func.sigma.setValue(1.0, vmin=1e-20, vmax=20.0)
+else:
+    psf_func.fwhm.setValue(3.0, vmin=1e-20, vmax=20.0)
+if func == 'Moffat':
+    if beta4:
+        psf_func.beta.setValue(4.0, fixed=True)
+    else:
+        psf_func.beta.setValue(2.0, vmin=1.0, vmax=20.0)
+
 model = SimpleModelDescription()
 model.x0.setValue(psfradius)
-model.x0.setTolerance(0.2)
+model.x0.setLimitsRel(3, 3)
 model.y0.setValue(psfradius)
-model.y0.setTolerance(0.2)
-model.addFunction(moffat)
+model.y0.setLimitsRel(3, 3)
+model.addFunction(psf_func)
+
+print 'Initial PSF model:'
+print model
 
 psfmodels = []
 psfflags = []
 goodfraction = []
 chi2 = []
 for i in xrange(psfflux.shape[0]):
-    print 'Fitting PSF %d of %d' % (i, fill_flux.shape[0])
+    wl = psf_wl[i]
+    pf = psfflux[i] - psfbg[i]
+    pf[pf <= 0] = np.ma.masked
+    print 'Fitting PSF %d of %d (%s \AA)' % (i, fill_flux.shape[0], wl)
     imfit = Imfit(model, quiet=True)
-    imfit.fit(psfflux[i] - psfbg[i], mode='NM', use_model_for_errors=True)
+    imfit.fit(pf, mode='NM', use_model_for_errors=False)
     _goodfraction = float(imfit.nValidPixels) / float(psfflux[i].size)
     goodfraction.append(_goodfraction)
-    psfflags.append(not imfit.fitConverged)
+    flagged = not imfit.fitConverged or _goodfraction < 0.5
+    psfflags.append(flagged)
+    print '    Fit statistic: %f' % imfit.fitStatistic
     print '    Reduced fit statistic: %f' % imfit.reducedFitStatistic
+    print '    Valid pix: %.1f %%' % (_goodfraction * 100.0)
+    print '    Flagged? %s' % flagged
     chi2.append(imfit.reducedFitStatistic)
-    psfmodels.append(imfit.getModelDescription())
+    fitmodel = imfit.getModelDescription()
+    modelimage = imfit.getModelImage()
+    psfmodels.append(fitmodel)
+    print fitmodel
+
+    if debug:
+        pf = psfflux[i] - psfbg[i]
+        norm = pf.max()
+        residual = pf - modelimage
+    
+        plt.ioff()
+        plt.clf()
+        fig = plt.figure(1, figsize=(10, 7))
+        gs = plt.GridSpec(2, 3, height_ratios=[2.0, 3.0])
+        ax = plt.subplot(gs[0,0])
+        im = ax.imshow(pf / norm, vmin=0.0, vmax=1.1, cmap='OrRd')
+        plt.colorbar(im, ax=ax)
+        ax.set_title('original')
+        ax = plt.subplot(gs[0,1])
+        im = ax.imshow(modelimage / norm, vmin=0.0, vmax=1.1, cmap='OrRd')
+        plt.colorbar(im, ax=ax)
+        ax.set_title('model')
+        
+        ax = plt.subplot(gs[0,2])
+        #res_max = max(np.abs(np.min(residual)), np.max(residual))
+        im = ax.imshow(residual / norm, vmin=-0.35, vmax=0.35, cmap='RdBu')
+        plt.colorbar(im, ax=ax)
+        ax.set_title('residual')
+        
+        bins = np.arange(0, psfradius)
+        bins_c = bins[:-1]
+        pa = fitmodel.psf.PA.value
+        ell = fitmodel.psf.ell.value
+        pa, ell = fix_PA_ell(pa, ell)
+        pa = np.pi * (pa + 90) / 180.0
+        ba = 1.0 - ell
+        x0 = fitmodel.x0.value - 1
+        y0 = fitmodel.y0.value - 1
+        f_r = radialProfile(pf / norm, bins, x0, y0, pa, ba)
+        err_r = radialProfile(pf / norm, bins, x0, y0, pa, ba, mode='std')
+        model_f_r = radialProfile(modelimage / norm, bins, x0, y0, pa, ba)
+        residual_r = radialProfile(residual / norm, bins, x0, y0, pa, ba)
+         
+        if func == 'Gaussian':
+            fwhm = sigma2fwhm * fitmodel.psf.sigma.value
+        else:
+            fwhm = fitmodel.psf.fwhm.value
+    
+        ax = plt.subplot(gs[1,:])
+        ax.errorbar(bins_c, f_r, err_r, linestyle='-', color='k', ecolor='k', label='original')
+        ax.plot(bins_c, model_f_r, 'k--', label='model')
+        ax.plot(bins_c, residual_r, 'k:', label='residual')
+        ax.vlines(fwhm / 2, -0.1, 1.2, linestyles='dashdot')
+        ax.text(fwhm / 2 + 0.1, f_r.max() / 2 + 0.1, 'FWHM = %.3f "' % fwhm)
+        ax.legend(loc='upper right')
+        ax.set_ylim(-0.1, 1.2)
+        ax.set_xlim(-0.1, bins_c.max() + 0.1)
+
+        if beta4:
+            plt.suptitle(r'%s PSF fit with $\beta=4$ for %s (%s, $%d\,\AA$)' % (func, galaxy, grating, wl))
+        else:
+            plt.suptitle(r'%s PSF fit for %s (%s, $%d\,\AA$)' % (func, galaxy, grating, wl))
+    
+        gs.tight_layout(fig, rect=[0, 0, 1, 0.97])
+        plt.show()
 
 params = np.ma.empty(len(psfmodels), dtype=[('lambda', 'float64'), ('I_0', 'float64'),
                                             ('fwhm', 'float64'), ('beta', 'float64'), 
@@ -172,15 +267,23 @@ params = np.ma.empty(len(psfmodels), dtype=[('lambda', 'float64'), ('I_0', 'floa
                                             ('PA', 'float64'), ('ell', 'float64'),
                                             ('good', 'float64'), ('flag', 'bool'), ('chi2', 'float64')])
 
+psfflags = np.array(psfflags)
 params['lambda'] = psf_wl
-params['I_0'] = [m.Moffat.I_0.value for m in psfmodels]
-params['fwhm'] = [m.Moffat.fwhm.value for m in psfmodels]
-params['beta'] = [m.Moffat.beta.value for m in psfmodels]
+params['I_0'] = [m.psf.I_0.value for m in psfmodels]
+if func == 'Gaussian':
+    params['fwhm'] = [sigma2fwhm * m.psf.sigma.value for m in psfmodels]
+else:
+    params['fwhm'] = [m.psf.fwhm.value for m in psfmodels]
+if func == 'Moffat':
+    params['beta'] = [m.psf.beta.value for m in psfmodels]
+else:
+    params['beta'] = [0.0 for m in psfmodels]
 params['x0'] = [m.x0.value for m in psfmodels]
 params['y0'] = [m.y0.value for m in psfmodels]
+
 PA = []
 ell = []
-for _PA, _ell in zip([m.Moffat.PA.value for m in psfmodels], [m.Moffat.ell.value for m in psfmodels]):
+for _PA, _ell in zip([m.psf.PA.value for m in psfmodels], [m.psf.ell.value for m in psfmodels]):
     _PA, _ell = fix_PA_ell(_PA, _ell)
     PA.append(_PA)
     ell.append(_ell)
@@ -192,31 +295,58 @@ params['chi2'] = chi2
 params[psfflags] = np.ma.masked
 
 header =' '.join(params.dtype.names)
-np.savetxt('out/%s.%s.v1.5.PSF.dat' % (galaxy, cubeversion), params, header=header)
+np.savetxt('out/%s_%s.%s.v1.5.PSF.dat' % (name, galaxy, grating), params, header=header)
 
-plt.clf()
+def getstats(p, wei):
+    p_wei = np.sum(p * wei)
+    p_var = np.sum((p - p_wei)**2 * wei)
+    p_std = np.sqrt(p_var)
+    return p_wei, p_std
+
+wei = np.exp(-0.5 * params['chi2'] / params['chi2'].min())
+#wei = np.ones_like(params['fwhm'])
+wei /= np.sum(wei)
+fwhm_wei, fwhm_std = getstats(params['fwhm'], wei)
+beta_wei, beta_std = getstats(params['beta'], wei)
+ell_wei, ell_std = getstats(params['ell'], wei)
+x0_wei, x0_std = getstats(params['x0'], wei)
+y0_wei, y0_std = getstats(params['y0'], wei)
+
+print 'FWHM = %.3f +- %.3f' % (fwhm_wei, fwhm_std)
+if func == 'Moffat' and not beta4:
+    print 'beta = %.3f +- %.3f' % (beta_wei, beta_std)
+print 'ell = %.3f +- %.3f' % (ell_wei, ell_std)
+print 'x0 = %.3f +- %.3f' % (x0_wei, x0_std)
+print 'y0 = %.3f +- %.3f' % (y0_wei, y0_std)
+
+
+
+
 plt.figure(figsize=(8, 12))
 plt.subplot(511)
 plt.plot(psf_wl, params['fwhm'], '-k')
 #plt.xlabel(r'wavelength $[\AA]$')
 plt.gca().set_xticklabels([])
 plt.ylabel(r'FWHM $[arcsec]$')
-plt.ylim(0.0, 5.0)
+#plt.ylim(0.0, 5.0)
 plt.xlim(psf_wl.min(), psf_wl.max())
-fwhm_median = np.median(params['fwhm'])
-plt.hlines(fwhm_median, psf_wl.min(), psf_wl.max(), linestyles='dashed', colors='k')
-plt.text(psf_wl.min() + 100, fwhm_median + 0.075, '%.3f' % fwhm_median)
+plt.hlines(fwhm_wei, psf_wl.min(), psf_wl.max(), linestyles='dashed', colors='k')
+plt.hlines(fwhm_wei + fwhm_std, psf_wl.min(), psf_wl.max(), linestyles='dotted', colors='k')
+plt.hlines(fwhm_wei - fwhm_std, psf_wl.min(), psf_wl.max(), linestyles='dotted', colors='k')
+plt.text(psf_wl.min() + 100, fwhm_wei + 0.075, '%.3f' % fwhm_wei)
 
-plt.subplot(512)
-plt.plot(psf_wl, params['beta'], '-k')
-#plt.xlabel(r'wavelength $[\AA]$')
-plt.gca().set_xticklabels([])
-plt.ylabel(r'$\beta$')
-plt.ylim(0.0, 4.0)
-plt.xlim(psf_wl.min(), psf_wl.max())
-beta_median = np.median(params['beta'])
-plt.hlines(beta_median, psf_wl.min(), psf_wl.max(), linestyles='dashed', colors='k')
-plt.text(psf_wl.min() + 100, beta_median + 0.075, '%.3f' % beta_median)
+if func == 'Moffat' and not beta4:
+    plt.subplot(512)
+    plt.plot(psf_wl, params['beta'], '-k')
+    #plt.xlabel(r'wavelength $[\AA]$')
+    plt.gca().set_xticklabels([])
+    plt.ylabel(r'$\beta$')
+    #plt.ylim(0.0, 4.0)
+    plt.xlim(psf_wl.min(), psf_wl.max())
+    plt.hlines(beta_wei, psf_wl.min(), psf_wl.max(), linestyles='dashed', colors='k')
+    plt.hlines(beta_wei + beta_std, psf_wl.min(), psf_wl.max(), linestyles='dotted', colors='k')
+    plt.hlines(beta_wei - beta_std, psf_wl.min(), psf_wl.max(), linestyles='dotted', colors='k')
+    plt.text(psf_wl.min() + 100, beta_wei + 0.075, '%.3f' % beta_wei)
 
 plt.subplot(513)
 plt.plot(psf_wl, params['ell'], '-k')
@@ -225,28 +355,27 @@ plt.gca().set_xticklabels([])
 plt.ylabel(r'$\epsilon$')
 plt.ylim(0.0, 1.05)
 plt.xlim(psf_wl.min(), psf_wl.max())
-ell_median = np.median(params['ell'])
-plt.hlines(ell_median, psf_wl.min(), psf_wl.max(), linestyles='dashed', colors='k')
-plt.text(psf_wl.min() + 100, ell_median, '%.3f' % ell_median)
-
+plt.hlines(ell_wei, psf_wl.min(), psf_wl.max(), linestyles='dashed', colors='k')
+plt.hlines(ell_wei + ell_std, psf_wl.min(), psf_wl.max(), linestyles='dotted', colors='k')
+plt.hlines(ell_wei - ell_std, psf_wl.min(), psf_wl.max(), linestyles='dotted', colors='k')
+plt.text(psf_wl.min() + 100, ell_wei, '%.3f' % ell_wei)
 plt.subplot(514)
-plt.plot(psf_wl, PA, '-k')
+plt.plot(psf_wl, params['PA'], '-k')
 #plt.xlabel(r'wavelength $[\AA]$')
 plt.gca().set_xticklabels([])
 plt.ylabel(r'P.A. $[degrees]$')
 plt.ylim(0.0, 180.0)
 plt.xlim(psf_wl.min(), psf_wl.max())
-PA_median = np.median(params['PA'])
-plt.hlines(PA_median, psf_wl.min(), psf_wl.max(), linestyles='dashed', colors='k')
-plt.text(psf_wl.min() + 100, PA_median, '%.1f' % PA_median)
 
 plt.subplot(515)
-plt.plot(psf_wl, params['good'] * 100.0, '-k')
+plt.plot(psf_wl, params['chi2'], '-k')
 plt.xlabel(r'wavelength $[\AA]$')
-plt.ylabel(r'Good pixels $[\%]$')
-plt.ylim(0.0, 110.0)
+plt.ylabel(r'$\chi^2$')
 plt.xlim(psf_wl.min(), psf_wl.max())
 
-plt.suptitle('PSF Moffat parameters for %s (%s)' % (galaxy, cubeversion))
-plt.savefig('out/%s.%s.v1.5.PSF.png' % (galaxy, cubeversion))
+if beta4:
+    plt.suptitle(r'%s PSF parameters with $\beta=4$ for %s (%s)' % (func, galaxy, grating))
+else:
+    plt.suptitle('%s PSF parameters for %s (%s)' % (func, galaxy, grating))
+plt.savefig('out/%s_%s.%s.v1.5.PSF.png' % (name, galaxy, grating))
 
--- test/psf/psf_plot.py
+++ test/psf/psf_plot.py
@@ -6,6 +6,15 @@ Created on 10/09/2014
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+from os import path
+
+func = sys.argv[2] # 'Kolmogorov', 'Moffat, 'Gaussian'
+beta4 = len(sys.argv) > 3 and sys.argv[3] == 'beta4'
+if beta4:
+    name = '%sBeta4' % (func)
+else:
+    name = func
 
 galaxiesV500 = ['NGC0023',
                 'NGC2916',
@@ -39,28 +48,30 @@ param_dtype = [('lambda', 'float64'), ('I_0', 'float64'),
                ('fwhm', 'float64'), ('beta', 'float64'), 
                ('x0', 'float64'), ('y0', 'float64'),
                ('PA', 'float64'), ('ell', 'float64'),
-               ('good', 'float64'), ('flag', 'bool'), ('chi2', 'float64')]
+               ('good', 'float64'), ('flag', 'float64'), ('chi2', 'float64')]
 
-nlambdaV500 = 94
-nlambdaV1200 = 30
+nlambdaV500 = 10
+nlambdaV1200 = 3
 
 fwhmV500 = np.ma.empty((len(galaxiesV500),  nlambdaV500))
 betaV500 = np.ma.empty((len(galaxiesV500),  nlambdaV500))
 x0V500 = np.ma.empty((len(galaxiesV500),  nlambdaV500))
 y0V500 = np.ma.empty((len(galaxiesV500),  nlambdaV500))
 ellV500 = np.ma.empty((len(galaxiesV500),  nlambdaV500))
+chi2V500 = np.ma.empty((len(galaxiesV500),  nlambdaV500))
 
 fwhmV1200 = np.ma.empty((len(galaxiesV1200),  nlambdaV1200))
 betaV1200 = np.ma.empty((len(galaxiesV1200),  nlambdaV1200))
 x0V1200 = np.ma.empty((len(galaxiesV1200),  nlambdaV1200))
 y0V1200 = np.ma.empty((len(galaxiesV1200),  nlambdaV1200))
 ellV1200 = np.ma.empty((len(galaxiesV1200),  nlambdaV1200))
+chi2V1200 = np.ma.empty((len(galaxiesV1200),  nlambdaV1200))
 
 for i, galaxy in enumerate(galaxiesV500):
-    cube = 'out/%s.%s.v1.5.PSF.dat' % (galaxy, 'V500')
+    cube = path.join(sys.argv[1], '%s_%s.%s.v1.5.PSF.dat' % (name, galaxy, 'V500'))
     p = np.genfromtxt(cube, dtype=param_dtype)
     wlV500 = p['lambda']
-    mask = p['flag'] | (p['good'] < 0.6)
+    mask = p['flag'] > 0.0
     fwhmV500[i] = p['fwhm']
     fwhmV500[i, mask] = np.ma.masked
     betaV500[i] = p['beta']
@@ -71,12 +82,14 @@ for i, galaxy in enumerate(galaxiesV500):
     y0V500[i, mask] = np.ma.masked
     ellV500[i] = p['ell']
     ellV500[i, mask] = np.ma.masked
+    chi2V500[i] = p['chi2']
+    chi2V500[i, mask] = np.ma.masked
 
 for i, galaxy in enumerate(galaxiesV1200):
-    cube = 'out/%s.%s.v1.5.PSF.dat' % (galaxy, 'V1200')
+    cube = 'out/%s_%s.%s.v1.5.PSF.dat' % (name, galaxy, 'V1200')
     p = np.genfromtxt(cube, dtype=param_dtype)
     wlV1200 = p['lambda']
-    mask = p['flag'] | (p['good'] < 0.7)
+    mask = p['flag'] > 0.0
     fwhmV1200[i] = p['fwhm']
     fwhmV1200[i, mask] = np.ma.masked
     betaV1200[i] = p['beta']
@@ -87,6 +100,8 @@ for i, galaxy in enumerate(galaxiesV1200):
     y0V1200[i, mask] = np.ma.masked
     ellV1200[i] = p['ell']
     ellV1200[i, mask] = np.ma.masked
+    chi2V1200[i] = p['chi2']
+    chi2V1200[i, mask] = np.ma.masked
 
 
 wlmin = wlV1200.min()
@@ -102,33 +117,69 @@ for g in galaxiesV1200:
     x = psfcenter[g]
     distV1200.append(np.sqrt((x[0] - imcenter[0])**2 + (x[1] - imcenter[1])**2))
 
+def getstats1(p, wei):
+    p_wei = np.sum(p * wei)
+    p_var = np.sum((p - p_wei)**2 * wei)
+    p_std = np.sqrt(p_var)
+    return p_wei, p_std
+
+def getstats(p, wei):
+    p_wei = np.sum(p * wei, axis=0)
+    p_var = np.sum((p - p_wei)**2 * wei, axis=0)
+    p_std = np.sqrt(p_var)
+    return p_wei, p_std
+
+weiV500 = np.exp(-0.25 * chi2V500 / chi2V500.min())
+weiV500 /= np.sum(weiV500, axis=0)
+weiV500_1 = weiV500 / weiV500.sum()
+weiV1200 = np.exp(-0.25 * chi2V1200 / chi2V1200.min())
+weiV1200 /= np.sum(weiV1200, axis=0)
+weiV1200_1 = weiV1200 / weiV1200.sum()
+
+fwhmV500_wei, fwhmV500_std = getstats(fwhmV500, weiV500)
+betaV500_wei, betaV500_std = getstats(betaV500, weiV500)
+fwhmV1200_wei, fwhmV1200_std = getstats(fwhmV1200, weiV1200)
+betaV1200_wei, betaV1200_std = getstats(betaV1200, weiV1200)
+
 plt.clf()
 plt.subplot(211)
-plt.plot(wlV500, fwhmV500.mean(axis=0), 'r-')
-plt.plot(wlV500, fwhmV500.mean(axis=0) - fwhmV500.std(axis=0), 'r--')
-plt.plot(wlV500, fwhmV500.mean(axis=0) + fwhmV500.std(axis=0), 'r--')
-plt.plot(wlV1200, fwhmV1200.mean(axis=0), 'b-')
-plt.plot(wlV1200, fwhmV1200.mean(axis=0) + fwhmV1200.std(axis=0), 'b--')
-plt.plot(wlV1200, fwhmV1200.mean(axis=0) - fwhmV1200.std(axis=0), 'b--')
+plt.plot(wlV500, fwhmV500_wei, 'r-')
+plt.plot(wlV500, fwhmV500_wei - fwhmV500_std, 'r--')
+plt.plot(wlV500, fwhmV500_wei + fwhmV500_std, 'r--')
+plt.plot(wlV1200, fwhmV1200_wei, 'b-')
+plt.plot(wlV1200, fwhmV1200_wei + fwhmV1200_std, 'b--')
+plt.plot(wlV1200, fwhmV1200_wei - fwhmV1200_std, 'b--')
+plt.text(6000, 3.5, r'$\mathrm{FWHM(V500)}=%.3f \pm %.3f$' % getstats1(fwhmV500, weiV500_1))
+plt.text(4000, 3.5, r'$\mathrm{FWHM(V1200)}=%.3f \pm %.3f$' % getstats1(fwhmV1200, weiV1200_1))
 plt.ylabel(r'FWHM $[arcsec]$')
-#plt.xlabel(r'wavelength $[\AA]$')
-plt.gca().set_xticklabels([])
+if func != 'Moffat' or beta4:
+    plt.xlabel(r'wavelength $[\AA]$')
+else:
+    plt.gca().set_xticklabels([])
 plt.ylim(0.0, 5.0)
 plt.xlim(wlmin, wlmax)
 
-plt.subplot(212)
-plt.plot(wlV500, betaV500.mean(axis=0), 'r-')
-plt.plot(wlV500, betaV500.mean(axis=0) - betaV500.std(axis=0), 'r--')
-plt.plot(wlV500, betaV500.mean(axis=0) + betaV500.std(axis=0), 'r--')
-plt.plot(wlV1200, betaV1200.mean(axis=0), 'b-')
-plt.plot(wlV1200, betaV1200.mean(axis=0) + betaV1200.std(axis=0), 'b--')
-plt.plot(wlV1200, betaV1200.mean(axis=0) - betaV1200.std(axis=0), 'b--')
-plt.ylabel(r'$\beta$')
-plt.xlabel(r'wavelength $[\AA]$')
-plt.ylim(0.0, 4.0)
-plt.xlim(wlmin, wlmax)
+if func == 'Moffat' and not beta4:
+    plt.subplot(212)
+    plt.plot(wlV500, betaV500_wei, 'r-')
+    plt.plot(wlV500, betaV500_wei - betaV500_std, 'r--')
+    plt.plot(wlV500, betaV500_wei + betaV500_std, 'r--')
+    plt.plot(wlV1200, betaV1200_wei, 'b-')
+    plt.plot(wlV1200, betaV1200_wei + betaV1200_std, 'b--')
+    plt.plot(wlV1200, betaV1200_wei - betaV1200_std, 'b--')
+    plt.text(6000, 3.5, r'$\beta\mathrm{(V500)}=%.3f \pm %.3f$' % getstats1(betaV500, weiV500_1))
+    plt.text(4000, 3.5, r'$\beta\mathrm{(V500)}=%.3f \pm %.3f$' % getstats1(betaV500, weiV500_1))
+    plt.ylabel(r'$\beta$')
+    plt.xlabel(r'wavelength $[\AA]$')
+    plt.ylim(0.0, 4.0)
+    plt.xlim(wlmin, wlmax)
 
-plt.savefig('out/PSF_all.png')
+if func == 'Moffat' and beta4:
+    plt.suptitle(r'%s | $\beta=4$' % func)
+else:
+    plt.suptitle(r'%s' % func)
+
+plt.savefig(path.join(sys.argv[1], '%s_PSF_all.png' % name))
 
 
 plt.figure(figsize=(5,6))
@@ -144,11 +195,23 @@ plt.ylim(0.0,5.0)
 plt.xlim(0.0,35.0)
 plt.legend(loc='lower right')
 
-plt.subplot(212)
-plt.plot(distV500, betaV500.mean(axis=1), 'ro', label='V500')
-plt.plot(distV1200, betaV1200.mean(axis=1), 'bo', label='V1200')
-plt.ylabel(r'$\beta$')
-plt.xlabel(r'distance $[arcsec]$')
-plt.ylim(0.0,5.0)
-plt.xlim(0.0,35.0)
-plt.savefig('out/PSF_distance.png')
+if func == 'Moffat' and not beta4:
+    plt.subplot(212)
+    plt.plot(distV500, betaV500.mean(axis=1), 'ro', label='V500')
+    plt.plot(distV1200, betaV1200.mean(axis=1), 'bo', label='V1200')
+    plt.ylabel(r'$\beta$')
+    plt.xlabel(r'distance $[arcsec]$')
+    plt.ylim(0.0,5.0)
+    plt.xlim(0.0,35.0)
+
+plt.savefig(path.join(sys.argv[1], '%s_PSF_distance.png' % name))
+
+print 'Summary:'
+print 'fwhm(V500) = %.3f +- %.3f' % getstats1(fwhmV500, weiV500_1)
+print 'fwhm(V1200) = %.3f +- %.3f' % getstats1(fwhmV1200, weiV1200_1)
+if func == 'Moffat' and not beta4:
+    print 'beta(V500) = %.3f +- %.3f' % getstats1(betaV500, weiV500_1)
+    print 'beta(V1200) = %.3f +- %.3f' % getstats1(betaV1200, weiV1200_1)
+print 'ell(V500) = %.3f +- %.3f' % getstats1(ellV500, weiV500_1)
+print 'ell(V1200) = %.3f +- %.3f' % getstats1(ellV1200, weiV1200_1)
+
