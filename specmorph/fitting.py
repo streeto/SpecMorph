e128b255b8e3bbb6114a57d4273771ca1e235028
André Luiz de Amorim <streetomon@gmail.com>
2015-04-10 15:01:20 -0300

    Put explicit in the code that the fitting is done using 'sigma' weights. Also, print and return reduced fit statistics when available.

--- specmorph/fitting.py
+++ specmorph/fitting.py
@@ -16,22 +16,28 @@ def fit_image(flux, noise, guess_model, PSF=None,
     Doc me!
     '''
     imfit = Imfit(guess_model, PSF, quiet=quiet, nproc=nproc)
-    imfit.fit(flux, noise, mode=mode, use_cash_statistics=(mode != 'LM' and use_cash_statistics))
-    logger.debug('Valid pix: %d | Iterations: %d | pegged: %d | fit statistic: %f' % \
-                 (imfit.nValidPixels, imfit.nIter, imfit.nPegged, imfit.fitStatistic))
+    imfit.fit(flux, noise,
+              mode=mode,
+              error_type='sigma',
+              use_cash_statistics=(mode != 'LM' and use_cash_statistics))
+    logger.debug('Valid pix: %d | Iter.: %d | pegged: %d | fit stat.: %f | BIC: %f' % \
+                 (imfit.nValidPixels, imfit.nIter, imfit.nPegged, imfit.reducedFitStatistic, imfit.BIC))
     fitted_model = imfit.getModelDescription()
     if not imfit.fitConverged or imfit.nPegged > 0:
         logger.warn('Bad fit: did not converge or pegged parameter.')
         if mode == 'LM' and insist:
             logger.warn('     Retrying using N-M simplex.')
-            imfit.fit(flux, noise, mode='NM', use_cash_statistics=use_cash_statistics)
+            imfit.fit(flux, noise,
+                      mode='NM',
+                      error_type='sigma',
+                      use_cash_statistics=use_cash_statistics)
             fitted_model = imfit.getModelDescription()
             if imfit.fitConverged:
-                logger.debug('     N-M simplex fit statistic: %f' % imfit.fitStatistic)
+                logger.debug('     N-M simplex fit statistic: %f | BIC: %f' % (imfit.reducedFitStatistic, imfit.BIC))
             else:
                 logger.warn('     Bad fit: N-M simplex did not converge.')
                 logger.debug('     Initial model:\n%s\n\n' % str(guess_model))
-    return fitted_model, imfit.fitConverged, imfit.fitStatistic
+    return fitted_model, imfit.fitConverged, imfit.reducedFitStatistic
 ################################################################################
 
 
